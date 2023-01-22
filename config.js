6d6c6dc9b3da70d759be4b4f28b12c045bf71a72
JustinChungCYT <0921justin@gmail.com>
2023-01-22 15:04:14 -0600

    Modified config

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
+const sequelize = new Sequelize(process.env.MYSQL_URI
+||
     'tutorial',
     process.env.USERNAME || 'root',
     process.env.PASSWORD || '1234567',
